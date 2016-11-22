d5bfb3a8c410876df36ff0597f206ba4915152f8
Jonny Pickard <jonathandpickard@gmail.com>
2016-11-22 17:33:53 +0000

    Initial Commit

---
+++ travel-republic/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  travel-republic
+//
+//  Created by Jonny Pickard on 22/11/2016.
+//  Copyright © 2016 Jonny Pickard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
